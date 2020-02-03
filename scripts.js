ad1424858599f904217afd0a1f50330a8677d496
Lawrence Ogri <lawrenceogri@yahoo.com>
2020-02-03 11:13:57 +0100

    Add localStorage functionality

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ const ul = document.querySelector('ul')
 const button = document.querySelector('button')
 const input = document.getElementById('item')
 
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
 const liMaker = text => {
     const li = document.createElement('li')
     li.textContent = text
@@ -12,7 +17,15 @@ const liMaker = text => {
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
+    itemsArray.push(input.value)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
     liMaker(input.value)
     input.value = ''
 })
 
+button.addEventListener('click', function(){
+    localStorage.clear()
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild)
+    }
+})
